0989298139a5b928cafaee01208799d12b37baa6
Khasanov-developer <khasss123@yandex.ru>
2019-09-28 17:39:45 +0500

    web_hello_django

---
+++ lab2_web_app/lab2_web_app/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path, include
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # when client request http://127.0.0.1:8000/index/, the project will find the mapped process method in my_hello_world app's urls.py file.
+    path('index/', include('hello_world_app.urls'))
+]
---
+++ lab2_web_app/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Desktop\Istomin\Веб\web_programming\lab2_web_app\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
