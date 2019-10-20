5f71ea3bc39095992c13685ab311c13f5f5e2a91
Khasanov-developer <khasss123@yandex.ru>
2019-10-20 15:59:57 +0500

    added web_app v2

--- web_app/orders/models.py
+++ web_app/orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from django.db.models.signals import post_save
 # Create your models here.
 
 class Status(models.Model):
@@ -33,6 +34,9 @@ class Order(models.Model):
         verbose_name = 'Заказ'
         verbose_name_plural = 'Заказы'
 
+    def save(self, *args, **kwargs):
+        super(Order, self).save(*args, **kwargs)
+
 class ProductInOrder(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, blank=True, null = True, default=None)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True, null = True, default=None)
@@ -47,5 +51,23 @@ class ProductInOrder(models.Model):
         return '%s' % self.product.name
 
     class Meta:
-        verbose_name = 'Товар'
-        verbose_name_plural = 'Товары'
+        verbose_name = 'Товар в заказе'
+        verbose_name_plural = 'Товары в заказе'
+
+    def save(self, *args, **kwargs):
+        self.price_per_item = self.product.price
+        self.total_price = self.number * self.price_per_item
+        super(ProductInOrder, self).save(*args, **kwargs)
+
+
+def product_in_order_post_save(sender, instance, created, **kwargs):
+    all_products_in_order = ProductInOrder.objects.filter(order = instance.order, is_active = True)
+    order_total_price = 0
+
+    for item in all_products_in_order:
+        order_total_price += item.total_price
+
+    instance.order.total_price = order_total_price
+    instance.order.save(force_update=True)
+
+post_save.connect(product_in_order_post_save, sender = ProductInOrder)
